f8708c669c9f1fec8fa398e34cf22cc75f405fe4
RoyLowe <roylowe96@gmail.com>
2021-08-25 21:09:32 -0400

    Navigation Part 3

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { Card} from 'react-native-elements';
+import { CAMPSITES, Campsites } from '../shared/campsites';
+import { PARTNERS, Partners } from '../shared/partners';
+import { PROMOTIONS, Promotions } from '../shared/promotions';
+
+function RenderItem({item}){
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/react-lake.jpg')}>
+                <Text
+                    style={{maargin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        };
+    }
 
     static navigationOptions = {
         title: 'Home'
@@ -9,9 +38,14 @@ class Home extends Component {
 
     render() {
         return (
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItem
+                    item={this.state.campsites.filter(campsite => campsite.featured)[0]} />
+                <RenderItem
+                    item={this.state.promotions.filter(promotion => promotion.featured)[0]} />
+                <RenderItem
+                    item={this.state.partners.filter(partner => partner.featured)[0]} />
+            </ScrollView>
         );
     }
 }
