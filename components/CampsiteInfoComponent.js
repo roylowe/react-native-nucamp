c509d7089f8b3b3896982bf4e620b8e137a2ae85
RoyLowe <roylowe96@gmail.com>
2021-09-13 20:26:33 -0400

    Secure Store

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -28,6 +28,7 @@ function RenderCampsite(props) {
     const view = React.createRef();
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
@@ -56,9 +57,17 @@ function RenderCampsite(props) {
                     { cancelable: false }
                 );
             }
+            else if (recognizeComment(gestureState)) {
+                props.onShowModal();
+
+            }
+
+
             return true;
         }
     });
+
+
 
     if (campsite) {
         return (
